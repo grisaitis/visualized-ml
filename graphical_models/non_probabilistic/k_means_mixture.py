6ed672855e0f851d31ced590ce333e0645e44ef4
William C Grisaitis <wgrisaitis@gmail.com>
2020-04-24 00:00:11 -0400

    make k-means assignment way faster with jax.jit

--- graphical_models/non_probabilistic/k_means_mixture.py
+++ graphical_models/non_probabilistic/k_means_mixture.py
@@ -8,14 +8,7 @@ class KMeansMixture:
         self.means = means
 
     def assign(self, x):
-        n = x.shape[0]
-        distances = jax.numpy.abs(x[:, None] - self.means[None, :])
-        assert distances.shape == (n, self.k), distances.shape
-        assignment_indices = jax.numpy.argmin(distances, axis=1)
-        assert assignment_indices.shape == x.shape, assignment_indices.shape
-        assignments = jax.numpy.identity(self.k)[assignment_indices]
-        assert assignments.shape == (n, self.k), assignments.shape
-        return assignments
+        return _assign(x, self.means)
 
     @classmethod
     def learn_with_lloyds_algorithm(cls, key, k_means, x):
@@ -39,8 +32,20 @@ class KMeansMixture:
             print("means", means_t)
             change_magnitude = jax.numpy.linalg.norm(means_t - means_t_minus_1)
             print("change_magnitude", change_magnitude)
-            if change_magnitude < 0.0001:
+            if change_magnitude < 1e-8:
                 break
             means_t_minus_1 = means_t
 
         return cls(means_t)
+
+
+@jax.jit
+def _assign(x, means):
+    n, k = x.shape[0], means.shape[0]
+    distances = jax.numpy.abs(x[:, None] - means[None, :])
+    assert distances.shape == (n, k), distances.shape
+    assignment_indices = jax.numpy.argmin(distances, axis=1)
+    assert assignment_indices.shape == x.shape, assignment_indices.shape
+    assignments = jax.numpy.identity(k)[assignment_indices]
+    assert assignments.shape == (n, k), assignments.shape
+    return assignments
